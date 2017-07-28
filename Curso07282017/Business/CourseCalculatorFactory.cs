dbcf3cb924c77b994fa91d403e95d9f7072f8371
Cinthya Landa <cinthya.landa@gmail.com>
2017-07-28 16:49:20 -0700

    csproj

--- Curso07282017/Business/CourseCalculatorFactory.cs
+++ Curso07282017/Business/CourseCalculatorFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Business
 {
-    class CourseCalculatorFactory
+    public class CourseCalculatorFactory
     {
+        private Dictionary<Type, GradeCalculator> Container { get; set; }
+
+        public CourseCalculatorFactory()
+        {
+            Container = new Dictionary<Type, GradeCalculator>();
+        }
     }
 }
