a2cd96c63f7c828c752e167a24383d2d0acf0051
Gustavo <gustavomungarro@gmail.com>
2017-07-28 17:45:05 -0700

    MathCourse's criterias initialized via constructor.

--- Curso07282017/Models/MathCourse.cs
+++ Curso07282017/Models/MathCourse.cs
@@ -11,5 +11,13 @@ namespace Models
         public int Id { get; set; }
         public decimal ExcercisesCriteria { get; set; }
         public decimal ParticipationCriteria { get; set; }
+
+        public MathCourse(decimal ExamenCriteria, decimal HomeworkCriteria, decimal AttendanceCriteria, decimal ExcercisesCriteria, decimal ParticipationCriteria){
+            this.ExamenCriteria = ExamenCriteria;
+            this.HomeworkCriteria = HomeworkCriteria;
+            this.AttendanceCriteria = AttendanceCriteria;
+            this.ExcercisesCriteria = ExcercisesCriteria;
+            this.ParticipationCriteria = ParticipationCriteria;
+        }                          
     }
 }
