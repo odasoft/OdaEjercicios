d3c9c2dfb66fa6385a85eca7650015023f064410
Alejandro Beltran <alejandro.beltran@odasoft.com.mx>
2017-07-28 17:23:08 -0700

    PhysicCourse1

---
+++ Curso07282017/Models/PhysicCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class PhysicCalculator : IGradeCalculator
+    {
+        public decimal CalculateGrade(StudentGrades studentGrade)
+        {
+            PhysicStudentGrade physicStudentGrade = studentGrade as PhysicStudentGrade;
+
+            return 
+        }
+    }
+}
