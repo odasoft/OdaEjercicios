bd279a54be83a2cee994bd6b865dca4ae3615f43
Alejandro Beltran <alejandro.beltran@odasoft.com.mx>
2017-08-11 17:01:32 -0700

    Physic contructors

--- Curso07282017/Models/PhysicStudentGrade.cs
+++ Curso07282017/Models/PhysicStudentGrade.cs
@@ -9,5 +9,18 @@ namespace Models
     public class PhysicStudentGrade: StudentGrades
     {
         public decimal PresentationGrade { set; get; }
+        public PhysicCourse Course { set; get; }
+
+
+        public PhysicStudentGrade(decimal ExamenGrade, decimal AttendanceGrade, decimal HomeWorkGrade, decimal PresentationGrade, PhysicCourse Course) {
+            this.ExamenGrade = ExamenGrade;
+            this.AttendanceGrade = AttendanceGrade;
+            this.HomeWorkGrade = HomeWorkGrade;
+            this.PresentationGrade = PresentationGrade;
+            this.Course = Course;
+
+        }
+
+
     }
 }
