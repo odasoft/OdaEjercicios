2ceca28ddeef1569434169c17568b0141415937c
Jorge Luis Renteria Zules <Jorge_rt100@hotmail.com>
2017-08-11 17:06:02 -0700

    add course History

---
+++ Curso07282017/Models/HistoryCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class HistoryCalculator : IGradeCalculator
+    {
+        public decimal CalculateGrade(StudentGrades studentGrade)
+        {
+            HistoryStudentGrade historyStudentGrade = studentGrade as HistoryStudentGrade;
+            HistoryCourse historyCourse = studentGrade.Course as HistoryCourse;
+
+            return historyCourse.AttendanceCriteria * historyStudentGrade.AttendanceGrade +
+                historyCourse.ExamenCriteria + historyStudentGrade.ExamenGrade +
+                historyCourse.ExcercisesCriteria * historyStudentGrade.ExcerciseGrade +
+                historyCourse.HomeworkCriteria * historyStudentGrade.HomeWorkGrade +
+                historyCourse.ParticipationCriteria * historyStudentGrade.ParticipationGrade;
+        }
+    }
+}
---
+++ Curso07282017/Models/HistoryCourse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class HistoryCourse : GradeableCourse
+    {
+        public int Id { get; set; }
+        public decimal ExcercisesCriteria { get; set; }
+        public decimal ParticipationCriteria { get; set; }
+
+        public HistoryCourse(decimal ExamenCriteria, decimal HomeworkCriteria, decimal AttendanceCriteria,
+            decimal ExcercisesCriteria, decimal ParticipationCriteria)
+        {
+            this.ExamenCriteria = ExamenCriteria;
+            this.HomeworkCriteria = HomeworkCriteria;
+            this.AttendanceCriteria = AttendanceCriteria;
+            this.ExcercisesCriteria = ExcercisesCriteria;
+            this.ParticipationCriteria = ParticipationCriteria;
+        }
+    }
+}
